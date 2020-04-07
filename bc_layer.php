886d6dcc6dcc99e256db947493b497664afe2268
Christoph Kappestein <christoph.kappestein@artemeon.de>
2020-04-07 11:50:01 +0200

    add mbstring and autoload bc layer which sets the fitting class aliases

---
+++ bc_layer.php
@@ -0,0 +1,7 @@
+<?php
+
+class_alias(\Artemeon\Database\Schema\DataType::class, 'Kajona\System\System\DbDatatypes');
+class_alias(\Artemeon\Database\Schema\Table::class, 'Kajona\System\System\Db\Schema\Table');
+class_alias(\Artemeon\Database\Schema\TableColumn::class, 'Kajona\System\System\Db\Schema\TableColumn');
+class_alias(\Artemeon\Database\Schema\TableIndex::class, 'Kajona\System\System\Db\Schema\TableIndex');
+class_alias(\Artemeon\Database\Schema\TableKey::class, 'Kajona\System\System\Db\Schema\TableKey');
