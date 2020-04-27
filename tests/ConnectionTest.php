61f4fe89af8fa51caeb7454e532c077506670ffe
Christoph Kappestein <christoph.kappestein@artemeon.de>
2020-04-27 18:20:30 +0200

    add dummy test case

---
+++ tests/ConnectionTest.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the Artemeon Core - Web Application Framework.
+ *
+ * (c) Artemeon <www.artemeon.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Artemeon\Database\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @since 7.3
+ */
+class ConnectionTest extends TestCase
+{
+    public function testConnection()
+    {
+        $this->assertTrue(true);
+    }
+}
