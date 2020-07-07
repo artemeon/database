8644728f028723fde717acd4ec8c0d74216aa074
Christoph Kappestein <christoph.kappestein@artemeon.de>
2020-07-07 15:37:03 +0200

    update test change column type from int to bigint

--- tests/ConnectionTest.php
+++ tests/ConnectionTest.php
@@ -101,12 +101,13 @@ class ConnectionTest extends ConnectionTestCase
         $connection->insert(self::TEST_TABLE_NAME, array('temp_id' => 'bbb', 'temp_int' => 222));
 
         $this->assertTrue($connection->hasColumn(self::TEST_TABLE_NAME, 'temp_id'));
+        $this->assertTrue($connection->hasColumn(self::TEST_TABLE_NAME, 'temp_int'));
         $this->assertTrue($connection->hasColumn(self::TEST_TABLE_NAME, 'temp_bigint'));
 
         $this->assertTrue($connection->changeColumn(self::TEST_TABLE_NAME, "temp_int", "temp_bigint_new", DataType::STR_TYPE_BIGINT));
 
         $this->assertTrue($connection->hasColumn(self::TEST_TABLE_NAME, 'temp_id'));
-        $this->assertFalse($connection->hasColumn(self::TEST_TABLE_NAME, 'temp_bigint'));
+        $this->assertFalse($connection->hasColumn(self::TEST_TABLE_NAME, 'temp_int'));
         $this->assertTrue($connection->hasColumn(self::TEST_TABLE_NAME, 'temp_bigint_new'));
 
         $row = $connection->getPRow("SELECT temp_id, temp_bigint_new FROM " . self::TEST_TABLE_NAME . " WHERE temp_id = ?", ['aaa']);
