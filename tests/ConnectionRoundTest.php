37c1a13cf90b8b1a658c7cdec2c9e1414b471c5f
Christoph Kappestein <christoph.kappestein@artemeon.de>
2021-04-15 17:14:44 +0200

    fix round test

--- tests/ConnectionRoundTest.php
+++ tests/ConnectionRoundTest.php
@@ -17,20 +17,20 @@ class ConnectionRoundTest extends ConnectionTestCase
 {
     public function testRound()
     {
-        $query = 'SELECT ROUND(0.33333333333333, 8) AS val FROM ' . self::TEST_TABLE_NAME;
+        $query = 'SELECT ROUND(1.33333333333333, 8) AS val FROM ' . self::TEST_TABLE_NAME;
         $row = $this->getConnection()->getPRow($query);
 
-        $this->assertEquals('0.33333333', substr((string) $row['val'], 0, 10));
-        $this->assertEqualsWithDelta(round(1 / 3, 8), (float) $row['val'], 0.0001);
+        $this->assertEquals('1.33333333', substr((string) $row['val'], 0, 10));
+        $this->assertEqualsWithDelta(1 + round(1 / 3, 8), (float) $row['val'], 0.0001);
     }
 
     public function testRoundUp()
     {
-        $query = 'SELECT ROUND(0.16666666666666, 8) AS val FROM ' . self::TEST_TABLE_NAME;
+        $query = 'SELECT ROUND(1.16666666666666, 8) AS val FROM ' . self::TEST_TABLE_NAME;
         $row = $this->getConnection()->getPRow($query);
 
-        $this->assertEquals('0.16666667', substr((string) $row['val'], 0, 10));
-        $this->assertEqualsWithDelta(round(1 / 6, 8), (float) $row['val'], 0.0001);
+        $this->assertEquals('1.16666667', substr((string) $row['val'], 0, 10));
+        $this->assertEqualsWithDelta(1 + round(1 / 6, 8), (float) $row['val'], 0.0001);
     }
 }
 
