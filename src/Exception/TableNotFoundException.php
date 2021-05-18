c9defc0ed8dc74b1b3ba3278c30e82761098f33b
Isik Battenstein <isik.battenstein@artemeon.de>
2021-05-18 12:07:17 +0200

    Updated TableNotFoundException.

--- src/Exception/TableNotFoundException.php
+++ src/Exception/TableNotFoundException.php
@@ -13,9 +13,9 @@ use Throwable;
 class TableNotFoundException extends \Exception
 {
 
-    public function __construct(string $message, string $table, Throwable $previous = null)
+    public function __construct(string $table, Throwable $previous = null)
     {
-        $this->message = $message.' '.$table;
+        $this->message = 'Table not found:  '.$table;
         parent::__construct($this->message, 0, $previous);
 
     }
