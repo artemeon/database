f8955b5493c46eb99f0c12530ef34efae9738d87
Christoph Kappestein <christoph.kappestein@artemeon.de>
2021-05-19 11:07:27 +0200

    throw exception in case we could not commit a transaction because of a nested rollback

--- src/Connection.php
+++ src/Connection.php
@@ -15,6 +15,7 @@ namespace Artemeon\Database;
 
 use Artemeon\Database\Exception\AddColumnException;
 use Artemeon\Database\Exception\ChangeColumnException;
+use Artemeon\Database\Exception\CommitException;
 use Artemeon\Database\Exception\ConnectionException;
 use Artemeon\Database\Exception\DriverNotFoundException;
 use Artemeon\Database\Exception\QueryException;
@@ -505,16 +506,17 @@ class Connection implements ConnectionInterface
         if ($this->objDbDriver != null) {
             //check, if the current tx is allowed to be commited
             if ($this->intNumberOfOpenTransactions == 1) {
+                //decrement counter
+                $this->intNumberOfOpenTransactions--;
+
                 //so, this is the last remaining tx. Commit or rollback?
                 if (!$this->bitCurrentTxIsDirty) {
                     $this->objDbDriver->transactionCommit();
                 } else {
                     $this->objDbDriver->transactionRollback();
                     $this->bitCurrentTxIsDirty = false;
+                    throw new CommitException('Could not commit transaction because a rollback occurred inside a nested transaction');
                 }
-
-                //decrement counter
-                $this->intNumberOfOpenTransactions--;
             } else {
                 $this->intNumberOfOpenTransactions--;
             }
