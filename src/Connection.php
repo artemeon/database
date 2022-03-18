3ccb6fa0d51c721320659571eda58254d37b3af7
Bernhard Grabietz <bernhard.grabietz@artemeon.de>
2022-03-18 11:30:04 +0100

    Update prettifyQuery to work with integer params
    cast in to string - since having integer params leads to "substr_replace(): Argument #2 ($replace) must be of type array|string, int given" error
    
    (cherry picked from commit 4b4a21bf6868b3f62968cf6314a84eef9ae8870f)

--- src/Connection.php
+++ src/Connection.php
@@ -1132,6 +1132,9 @@ class Connection implements ConnectionInterface
             if ($strOneParam === null) {
                 $strOneParam = 'null';
             }
+            if (is_int($strOneParam)) {
+                $strOneParam = (string) $strOneParam;
+            }
 
             $intPos = strpos($strQuery, '?');
             if ($intPos !== false) {
