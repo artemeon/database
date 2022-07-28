7a6b13e71148d5c14beb774b026f441c71e661f8
Christoph Kappestein <christoph.kappestein@artemeon.de>
2022-07-28 18:28:43 +0200

    #41 add table test

---
+++ tests/Schema/TableTest.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * This file is part of the Artemeon Core - Web Application Framework.
+ *
+ * (c) Artemeon <www.artemeon.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Artemeon\Database\Tests;
+
+use Artemeon\Database\Schema\Table;
+use Artemeon\Database\Schema\TableColumn;
+use Artemeon\Database\Schema\TableIndex;
+use Artemeon\Database\Schema\TableKey;
+use PHPUnit\Framework\TestCase;
+
+class TableTest extends TestCase
+{
+    public function testTable()
+    {
+        $table = new Table('foo');
+        $table->addColumn(new TableColumn('bar'));
+        $table->addIndex(new TableIndex('baz'));
+        $table->addPrimaryKey(new TableKey('pk'));
+
+        $expect = <<<JSON
+{
+  "name": "foo",
+  "indexes": [
+    {
+      "name": "baz",
+      "description": ""
+    }
+  ],
+  "keys": [
+    {
+      "name": "pk"
+    }
+  ],
+  "columns": [
+    {
+      "name": "bar",
+      "internalType": "",
+      "databaseType": "",
+      "nullable": true
+    }
+  ]
+}
+JSON;
+        $actual = json_encode($table);
+        $this->assertJsonStringEqualsJsonString($expect, $actual, $actual);
+    }
+}
