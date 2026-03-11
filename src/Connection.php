f65bdfca858b69620109f88a343782c58fbe5743
Marc Reichel <marc.reichel@artemeon.de>
2026-03-11 11:13:44 +0100

    #77 fix: PHPStan

--- src/Connection.php
+++ src/Connection.php
@@ -1222,7 +1222,7 @@ class Connection implements ConnectionInterface
      * An internal wrapper to dbsafeString, used to process a complete array of parameters
      * as used by prepared statements.
      *
-     * @param array<array-key, BackedEnum|EscapeableParameterInterface|scalar|null> $params
+     * @param array<array-key, BackedEnum|EscapeableParameterInterface|scalar|Stringable|null> $params
      * @param list<bool>|false $escapes An array of boolean for each param, used to block the escaping of html-special chars.
      *                                  If not passed, all params will be cleaned.
      *
